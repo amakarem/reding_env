58e1c253fefd19950f814d594acaf0677f170c5e
Ahmed Elaaser <44022058+amakarem@users.noreply.github.com>
2022-10-05 14:36:19 +0200

    Update readenv.php
    read value include =

--- readenv.php
+++ readenv.php
@@ -22,9 +22,27 @@ foreach ($envfile as $line) {
     if(strpos($line, '=') !== false) {
         $line = explode('=',$line);
         if(isset($line[1])) {
-            $env[$line[0]] = $line[1];
+            if(sizeof($line) > 2) {
+                $max = sizeof($line) - 1;
+                $x = 1;
+                while ($x < $max) {
+                    $x++;
+                    $line[1] = $line[1] .'='. $line[$x];
+                }
+            }
+            $env[$line[0]] = trim($line[1]);
         }
     }
 }
 
+$DBHost = $env['DB_HOST'];
+$DBName = $env['DB_DATABASE'];
+$DBLogin = $env['DB_USERNAME'];
+$DBPassword = $env['DB_PASSWORD'];
 
+global $DB;
+$DB = new mysqli($DBHost, $DBLogin, $DBPassword, $DBName);
+// Check connection
+if ($DB->connect_error) {
+    die("Connection failed: " . $DB->connect_error);
+}
